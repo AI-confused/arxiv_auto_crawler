1fe47999d538e69ac76053d5e008c11e9c5da2a4
AI-confused <21860437@zju.edu.cn>
2021-08-30 22:42:14 +0800

    2021-08-30:update downloadfile.py

--- src/arxiv_auto_crawler/download_file.py
+++ src/arxiv_auto_crawler/download_file.py
@@ -2,6 +2,7 @@ import pysftp
 import datetime
 import time
 import os
+from tqdm import tqdm
 import yaml
 import logging
 
@@ -44,7 +45,9 @@ class PaperDownloader(object):
         self.logger.setLevel(logging.INFO)
 
         # output to log file handler
-        file_handler = logging.FileHandler(os.path.join(self.log_dir, 'log-{}.log'.format(self.last_update_date)))
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir)
+        file_handler = logging.FileHandler(os.path.join(self.log_dir, 'log-{}.log'.format(self.log_date)))
         file_handler.setLevel(logging.INFO)
         file_handler.setFormatter(formatter)
 
@@ -87,17 +90,17 @@ class PaperDownloader(object):
         
         
     def paper_download(self):
-        sched_time = datetime.datetime.now() + datetime.timedelta(seconds=5)
+        sched_time = datetime.datetime.now() + datetime.timedelta(seconds=10)
         while True:
             now = datetime.datetime.now()
             if self.is_time_equal(now, sched_time):
                 self.logger.info('download time: ' + str(now))
                 daily_count = 0
 
-                with pysftp.Connection(host=self.hostname, username=self.username, password=self.password, cnopts=self.cnopts) as sftp:
+                with pysftp.Connection(host=self.hostname, port=self.port, username=self.username, password=self.password, cnopts=self.cnopts) as sftp:
                     self.logger.info('connection successful')
                     directory_list = self.check_files_exist(sftp, self.remote_path)
-                    if len(directory_list) > 1:
+                    if len(directory_list) > 0:
                         self.logger.info('start download pdf from remote server...')
 
                         remote_file_directory = self.remote_path + directory_list[0] + '/'
@@ -107,9 +110,9 @@ class PaperDownloader(object):
 
                             self.local_path = self.local_path + directory_list[0] + '/'
                             if not os.path.exists(self.local_path):
-                                os.mkdir(self.local_path)
+                                os.makedirs(self.local_path)
 
-                            for file in file_list:
+                            for file in tqdm(file_list, desc='download progress'):
                                 daily_count += 1
                                 remote_file_path = remote_file_directory + file
                                 local_file_path = self.local_path + file
@@ -119,16 +122,15 @@ class PaperDownloader(object):
                             self.logger.info('today count:' + str(daily_count))
                             self.logger.info('transfer finished')
                             time.sleep(20)
-                            for file in file_list:
+                            for file in tqdm(file_list, desc='delete remote server progress'):
                                 remote_file_path = remote_file_directory + file
                                 self.delete_files_from_server(sftp, file_path=remote_file_path)
+                            # delete remote_file_directory
                             self.delete_files_from_server(sftp, file_path=None, file_directory=remote_file_directory)
                             self.logger.info('delete finished')
-                            with open('log_download.txt', 'a') as f:
-                                f.write('delete finished\n')
                     else:
-                        self.logger.info('no update pdf')
-                sched_time = sched_time + datetime.timedelta(days=1)
-                self.logger.info('next download time: ' + str(sched_time))
+                        self.logger.info('no update pdf on remote server')
+                sched_time = time.time.now() + datetime.timedelta(seconds=60)
+                self.logger.info('next check time: ' + str(sched_time))
                 
                 
